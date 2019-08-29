82c1092645b4f6b206f548506bf161c1c5a3df8d
ilsubyeega <ilsubyeega@naver.com>
2019-08-29 14:45:39 +0900

    SEO: Add icon

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,27 @@
 
   ?>
   <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport"
+  
+  
+<link rel="apple-touch-icon" sizes="57x57" href="/assets/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/assets/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/assets/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/assets/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/assets/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/assets/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/assets/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/assets/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/assets/icon/png" sizes="192x192"  href="/assets/icon/android-icon-192x192.png">
+<link rel="icon" type="image/assets/icon/png" sizes="32x32" href="/assets/icon/favicon-32x32.png">
+<link rel="icon" type="image/assets/icon/png" sizes="96x96" href="/assets/icon/favicon-96x96.png">
+<link rel="icon" type="image/assets/icon/png" sizes="16x16" href="/assets/icon/favicon-16x16.png">
+<link rel="manifest" href="/assets/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/assets/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ef67b1">
+
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
