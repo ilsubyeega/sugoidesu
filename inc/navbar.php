acd4a5a680ef61eb1096655b2eb4736aa09f6198
ilsubyeega <ilsubyeega@naver.com>
2019-08-29 14:05:00 +0900

    Remove warning

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,9 +1,10 @@
+
 <body class="layout-3">
 	<div id="app">
 		<div class="main-wrapper container">
 			<div class="navbar-bg"></div>
 			<nav class="navbar navbar-expand-lg main-navbar">
-				<a href="index.html" class="navbar-brand sidebar-gone-hide">&nbsp; Leu</a>
+				<a href="https://leu.kr/" class="navbar-brand sidebar-gone-hide">&nbsp; Leu</a>
 
 				<div>
 					<ul class="navbar-nav">
@@ -21,40 +22,33 @@
 					<ul class="navbar-nav">
 					
 					
-						<li class="nav-item <? echo NavActive(Home, $navbar_active[2]); ?>">
+						<li class="nav-item <? echo NavActive("Home", $navbar_active[2]); ?>">
 							<a href="/" class="nav-link"><i class="far fa-heart"></i><span>Home</span></a>
 						</li>
 						
 						
-						<li class="nav-item <? echo NavActive(Leaderboards, $navbar_active[2]); ?> dropdown">
+						<li class="nav-item <? echo NavActive("Leaderboards", $navbar_active[2]); ?> dropdown">
 							<a data-toggle="dropdown" class="nav-link has-dropdown"><i class="far fa-chart-bar"></i><span>Leaderboards</span></a>
 							<ul class="dropdown-menu">
-								<li class="nav-item <? echo NavActive(LeadNormal, $navbar_active[3]); ?>"><a href="/leaderboards/normal" class="nav-link"><i class="fas fa-cookie-bite"></i>&nbsp; Normal</a></li>
+								<li class="nav-item <? echo NavActive("LeadNormal", $navbar_active[3]); ?>"><a href="/leaderboards/normal/std" class="nav-link"><i class="fas fa-cookie-bite"></i>&nbsp; Normal</a></li>
 								
-								<li class="nav-item <? echo NavActive(LeadRelax, $navbar_active[3]); ?>"><a href="/leaderboards/relax" class="nav-link"><i class="fas fa-cookie"></i>&nbsp; Relax</a></li>
+								<li class="nav-item <? echo NavActive("LeadRelax", $navbar_active[3]); ?>"><a href="/leaderboards/relax/std" class="nav-link"><i class="fas fa-cookie"></i>&nbsp; Relax</a></li>
 							</ul>
 						</li>
 						
 						
-						<li class="nav-item <? echo NavActive(Info, $navbar_active[2]); ?> dropdown">
+						<li class="nav-item <? echo NavActive("Info", $navbar_active[2]); ?> dropdown">
 							<a data-toggle="dropdown" class="nav-link has-dropdown"><i class="far fa-copy"></i><span>Info</span></a>
 							<ul class="dropdown-menu">
 							
-								<li class="nav-item <? echo NavActive(IJoinDiscord, $navbar_active[3]); ?>"><a href="https://discord.gg/jqKNUcT" class="nav-link"><i class="far fa-comment-alt"></i>&nbsp; Join Discord</a></li>
+								<li class="nav-item <? echo NavActive("IJoinDiscord", $navbar_active[3]); ?>"><a href="https://discord.gg/aC2G7dm" class="nav-link"><i class="far fa-comment-alt"></i>&nbsp; Join Discord</a></li>
 								
-								<li class="nav-item dropdown"><a class="nav-link has-dropdown">Documentation</a>
-									<ul class="dropdown-menu">
-										<li class="nav-item"><a href="/docs/what_is_Keesu" class="nav-link">What is Keesu?</a></li>
-										<li class="nav-item"><a href="/docs/How_to_connect" class="nav-link">How to connect</a></li>
-										<li class="nav-item"><a href="/docs/How_to_register" class="nav-link">How to register</a></li>
-										<li class="nav-item"><a href="/docs/Rank_the_Beatmaps" class="nav-link">Rank the Beatmaps</a></li>
-									</ul>
-								</li>
-								<li class="nav-item <? echo NavActive(ILICENSES, $navbar_active[3]); ?>"><a href="/licenses" class="nav-link">LICENSES</a></li>
+								<li class="nav-item <? echo NavActive("Docs", $navbar_active[3]); ?>"><a href="/docs" class="nav-link">Documentation</a></li>
+								<li class="nav-item <? echo NavActive("Licenses", $navbar_active[3]); ?>"><a href="/licenses" class="nav-link">LICENSES</a></li>
 							</ul>
 						</li>
-						<li class="nav-item">
-							<a href="/downloadswitcher" class="nav-link <? echo NavActive(DownloadSwitcher, $navbar_active[3]); ?>"><i class="far fa-snowflake"></i><span>Download Switcher</span></a>
+						<li class="nav-item <? echo NavActive("DownloadSwitcher", $navbar_active[2]); ?>">
+							<a href="/downloadswitcher" class="nav-link"><i class="far fa-snowflake"></i><span>Download Switcher</span></a>
 						</li>
 					</ul>
 				</div>
