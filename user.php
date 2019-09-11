0e40780b0325b3d9722dd0e9cd6be8f64d21b66d
ilsubyeega <ilsubyeega@naver.com>
2019-09-11 21:13:04 +0900

    make front better

--- user.php
+++ user.php
@@ -21,98 +21,247 @@ $navbar_active[1] = $config['global']['servername'];
 $navbar_active[2] = "User Information";
 $navbar_active[3] = "";
 
-$header['background_image'] = "../assets/img/unsplash/andre-benz-1214056-unsplash.jpg";
-$header['title'] = "User Information";
-$header['description'] = "";
 
 
 
 
 include "inc/header.php";
 include "inc/navbar.php";
-include "inc/base1.php";
 ?>
 
-
-
-
-            
-            <div class="row">
-              
-              <div class="col-12 col-md-12 col-lg-12">
-                <div class="card">
-                  <div class="card-body p-0">
-                    <div class="table-responsive">
-                      <table class="table table-striped table-md">
-                        <tbody><tr>
-                          <th><center>Rank</center></th>
-                          <th><center>Username</center></th>
-                          <th><center>pp</center></th>
-                          <th><center>Accuracy</center></th>
-                          <th><center>Playcount</center></th>
-                        </tr>
-                        <tr>
-                          <td><center>#1</center></td>
-                          <td><center><a href="/u/1000">ilsubyeega</a></center></td>
-                          <td><center><div class="badge badge-primary">7,652pp</div></center></center></td>
-                          <td><center><div class="badge badge-secondary">100%</div></center></td>
-                          <td><center><a href="#" class="badge badge-info">1,024</a></center></td>
-                        </tr>
-                        <tr>
-                          <td><center>#2</center></td>
-                          <td><center><a href="/u/1000">ilsubyeega</a></center></td>
-                          <td><center><div class="badge badge-primary">7,652pp</div></center></center></td>
-                          <td><center><div class="badge badge-secondary">100%</div></center></td>
-                          <td><center><a href="#" class="badge badge-info">1,024</a></center></td>
-                        </tr>
-                        <tr>
-                          <td><center>#3</center></td>
-                          <td><center><a href="/u/1000">ilsubyeega</a></center></td>
-                          <td><center><div class="badge badge-primary">7,652pp</div></center></center></td>
-                          <td><center><div class="badge badge-secondary">100%</div></center></td>
-                          <td><center><a href="#" class="badge badge-info">1,024</a></center></td>
-                        </tr>
-                        <tr>
-                          <td><center>#4</center></td>
-                          <td><center><a href="/u/1000">ilsubyeega</a></center></td>
-                          <td><center><div class="badge badge-primary">7,652pp</div></center></center></td>
-                          <td><center><div class="badge badge-secondary">100%</div></center></td>
-                          <td><center><a href="#" class="badge badge-info">1,024</a></center></td>
-                        </tr>
-                        <tr>
-                          <td><center>#5</center></td>
-                          <td><center><a href="/u/1000">ilsubyeega</a></center></td>
-                          <td><center><div class="badge badge-primary">7,652pp</div></center></center></td>
-                          <td><center><div class="badge badge-secondary">100%</div></center></td>
-                          <td><center><a href="#" class="badge badge-info">1,024</a></center></td>
-                        </tr>
-                      </tbody></table>
+<div class="main-content" style="min-height: 858px;">
+<div class="dropdown d-inline mr-2">
+                    <button class="btn btn-primary leu-disableshadow">Add Friend</button> &nbsp; 
+                      <button class="btn btn-primary dropdown-toggle leu-disableshadow" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Modes
+                      </button>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item active" href="#">Standard</a>
+                        <a class="dropdown-item" href="#">Taiko</a>
+                        <a class="dropdown-item" href="#">Catch The Beat</a>
+                        <a class="dropdown-item" href="#">Mania</a>
+                        <a class="dropdown-item" href="#">Standard (Relax)</a>
+                        <a class="dropdown-item" href="#">Taiko (Relax)</a>
+                        <a class="dropdown-item" href="#">Catch The Beat (Relax)</a>
+                      </div>
                     </div>
-                  </div>
-                  <div class="card-footer text-right">
-                    <nav class="d-inline-block">
-                      <ul class="pagination mb-0">
-                        <li class="page-item disabled">
-                          <a class="page-link" href="#" tabindex="-1"><i class="fas fa-chevron-left"></i></a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1 <span class="sr-only">(current)</span></a></li>
-                        <li class="page-item">
-                          <a class="page-link" href="#">2</a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                          <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                </div>
-              </div>
+                <br><br>
+    <!-- First -->
+    <section class="section">
+        <div class="hero text-white hero-bg-image" data-background="/assets/img/background/lune.jpg"
+            style="background-image: url(&quot;/assets/img/background/lune.jpg&quot;);">
+            <div class="hero-inner">
+                <h2>User Information</h2>
             </div>
-          </div>
-        </section>
-      </div>
+        </div>
 
-<?
+        <div class="card author-box card-primary">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="author-box-left">
+                            <img alt="image"
+                                src="https://cdn.discordapp.com/avatars/600605776381214738/7db15a83ccd19b7924294a9f877840cc.png"
+                                class="rounded-rectangle author-box-picture">
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="author-box-details">
+                            <div class="author-box-name">
+                                <h5>ilsubyeega </h5>
+                            </div>
+                            <div class="author-box-job">
+                                <span class="badge badge-info">Developer</span>
+                                <span class="badge badge-light">Bad Game Player</span>
+                            </div><br>
+                            Test<br><br>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        
+                    <div class="table-responsive">
+                        
+                      <table class="table table-sm">
+                        <tbody>
+                          <tr>
+                            <th scope="row">Rank</th>
+                            <td><center><span class="badge badge-primary">#1</span></center></td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Performance Point</th>
+                            <td><center><span class="badge badge-primary">100 pp</span></center></td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Hit Accuracy</th>
+                            <td><center><span class="badge badge-primary">100%</span></center></td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Play Count</th>
+                            <td><center><span class="badge badge-primary">1</span></center></td>
+                          </tr>
+                          </tr>
+                          <tr>
+                            <th scope="row">Total Score</th>
+                            <td><center><span class="badge badge-primary">3,000,000,010</span></center></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+</div>
+            <div class="card">
+                <div class="card-header">
+                    <h4>Best Scores</h4>
+                </div>
+                <div class="card-body p-0">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <th>
+                                        <center>Rank</center>
+                                    </th>
+                                    <th>
+                                        <center>Map</center>
+                                    </th>
+                                    <th>
+                                        <center>Score</center>
+                                    </th>
+                                    <th>
+                                        <center>Date</center>
+                                    </th>
+                                    <th>
+                                        <center>pp</center>
+                                    </th>
+                                    <th>
+                                        <center>Accuracy</center>
+                                    </th>
+                                    <th>
+                                        <center>Action</center>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <td class="p-0 text-center">
+                                        SS
+                                    </td>
+                                    <td><a href "#">yay i'm seunglabfx [yay you are being chicken]</a></td>
+                                    <td>1,100,100,100</td>
+                                    <td>
+                                        <center>
+                                            <a data-toggle="tooltip" title="" data-original-title="2019/9/8 20:21 AM">2
+                                                days ago</a>
+                                        </center>
+                                    </td>
+                                    <td>
+                                        <center>
+                                        <a data-toggle="tooltip" title="" data-original-title="100pp (100pp, 100%)">100pp</a>
+                                        </center>
+                                    </td>
+                                    <td>
+                                        <center>
+                                            100%
+                                        </center>
+                                    </td>
+                                    <td>
+                                        <center><button type="button" class="btn btn-primary" data-toggle="tooltip"
+                                                data-placement="top" title="" data-original-title="Download Replay">
+                                                <i class="fas fa-star"></i>
+                                            </button></center>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Recent Scores</h4>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <tbody>
+                                    <tr>
+                                        <th>
+                                            <center>Rank</center>
+                                        </th>
+                                        <th>
+                                            <center>Map</center>
+                                        </th>
+                                        <th>
+                                        <center>Score</center>
+                                    </th>
+                                        <th>
+                                            <center>Date</center>
+                                        </th>
+                                        <th>
+                                            <center>pp</center>
+                                        </th>
+                                        <th>
+                                            <center>Accuracy</center>
+                                        </th>
+
+                                        <th>
+                                            <center>Status</center>
+                                        </th>
+                                        <th>
+                                            <center>Action</center>
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <td class="p-0 text-center">
+                                            SS
+                                        </td>
+                                        <td><a href "#">yay i'm seunglabfx [yay you are being chicken]</a></td>
+                                        <td>1,100,100,100</td>
+                                        <td>
+                                            <center>
+                                                <a data-toggle="tooltip" title=""
+                                                    data-original-title="2019/9/8 20:21 AM">2 days ago</a>
+                                            </center>
+                                        </td>
+                                        <td>
+                                            <center>
+                                                100pp
+                                            </center>
+                                        </td>
+                                        <td>
+                                            <center>
+                                                100%
+                                            </center>
+                                        </td>
+
+                                        <td>
+                                            <center>
+                                                <div class="badge badge-success" data-toggle="tooltip"
+                                                    data-placement="top" title="" data-original-title="Completed"><i class="fas fa-check"></i></div>
+                                            </center>
+                                        </td>
+                                        <td>
+                                            <center><button type="button" class="btn btn-primary" data-toggle="tooltip"
+                                                    data-placement="top" title="" data-original-title="Download Replay">
+                                                    <i class="fas fa-star"></i>
+                                                </button></center>
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+            <?
 include "inc/footer.php";
 ?>
